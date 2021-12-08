a8e3e27fbc565b88fb5470cd4a48e9956f888838
Shane McElrath <shane@rangermac.com>
2021-12-07 20:52:50 -0500

    Wrote code that dynamicly creates Webpage using node

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-};
+const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
